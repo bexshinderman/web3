2465e7a290c9c81963a9c4b2709af6ba6d4183b2
shinrl1 <shinrl1@student.op.ac.nz>
2020-03-18 01:06:26 +1300

    dynamic urls

---
+++ Checkpoint_Sites/serving_static/flaskbackend.py
@@ -0,0 +1,45 @@
+
+
+from flask import Flask, request, render_template
+app = Flask(__name__,static_url_path='')
+
+
+@app.route('/load_data')
+def load_data():
+    return 'Success'
+
+
+
+@app.route('/index')
+@app.route('/home')
+@app.route('/')
+def index():
+    
+    return render_template("index.html")
+
+@app.route('/inspo')
+def inspo():
+    
+    return render_template("inspiration.html")
+
+@app.route('/page/<string:title>')
+def page(title):
+    return 'Title: ' + title
+   
+
+
+@app.route('/form')
+def form():
+    return render_template("form.html")
+@app.route('/response', methods=['POST'])
+def response():
+    name = request.form.get("name")
+    titletag = request.args.get('url')
+    return render_template("form.html", name=name)
+
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True,port=8080)
+
