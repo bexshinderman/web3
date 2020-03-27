20de1c60470d8c73731aac13686beee60174361c
shinrl1 <shinrl1@student.op.ac.nz>
2020-03-27 22:36:46 +1300

    for deployment

--- Checkpoint_Sites/serving_static/flaskbackend.py
+++ Checkpoint_Sites/serving_static/flaskbackend.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, Response, redirect, url_for
 app = Flask(__name__,static_url_path='')
 
 if __name__ =="__main__":
-    app.run(host='127.0.0.3', port=90)
+    app.run(host='127.0.0.3', port=5000)
 
 
 
